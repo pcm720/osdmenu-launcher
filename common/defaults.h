fa7400b213da93a585fa70699a017f35f269b868
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2025-03-31 01:38:12 -0700

    Update defaults.h

--- common/defaults.h
+++ common/defaults.h
@@ -4,8 +4,16 @@
 
 // All files must be placed on the memory card.
 // mc? paths are also supported.
+#ifndef CONF_PATH
 #define CONF_PATH "mc0:/OSDMENU/OSDMENU.CNF"
+#endif
+
+#ifndef LAUNCHER_PATH
 #define LAUNCHER_PATH "mc0:/OSDMENU/launcher.elf"
+#endif
+
+#ifndef DKWDRV_PATH
 #define DKWDRV_PATH "mc0:/PS1_DKWDRV/DKWDRV.ELF"
+#endif
 
 #endif
